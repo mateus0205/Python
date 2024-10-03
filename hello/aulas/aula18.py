0bdef7b56f4c019628d9ea960458f6b1e45f03c4
Mateus Henrique Martins <mateus.martins@sou.unifal-mg.edu.br>
2024-10-03 16:30:15 -0300

    alterações na aula 18

---
+++ hello/aulas/aula18.py
@@ -0,0 +1,12 @@
+pessoas = [['pedro',25],['maria',19],['joao',32]]
+print(pessoas[0][0]) # pega o indice da lista e depois da segunda lsita 
+                     # ou seja, vai imprimir (pedro)    
+
+teste = list()
+teste.append('Gustavo')
+teste.append(40)
+
+galera = list()
+galera.append(teste)
+print(galera)
+print(teste)
